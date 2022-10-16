661458bdd7c2aa77588df8c5dc9320e3c074bf01
thaheer_shaik <66290884+thaheershaik1018@users.noreply.github.com>
2022-10-16 16:14:09 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+export default function Home({ getData }) {  
+    return(
+        <section className="home">
+            <div className="content">
+                <p><br/><br/><br/>Click the button on the nav to get user details</p>
+            </div>
+            </section>
+    )
+}
